ddf90f59f699b267feefe54eeab56c87ebeee278
theoLassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-07-01 16:55:13 +0200

    feat: added home page

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="titles">
+            <h1>
+                Welcome To Valorant Lineups !
+            </h1>
+            <h2>
+                Select the map you want to practice :
+            </h2>
+        </div>
+    )
+}
+
+export default Home;
